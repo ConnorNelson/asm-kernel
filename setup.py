4c150d6013cfbfacd8abe33e81cec37a8cc521e2
Connor Nelson <Connor@ConnorNelson.com>
2021-03-17 21:45:07 -0700

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="asm_kernel",
     version="0.1",
-    python_requires=">=3.6"    
+    python_requires=">=3.6",
     packages=find_packages(),
     install_requires=["jupyter_client", "IPython", "ipykernel"],
 )
